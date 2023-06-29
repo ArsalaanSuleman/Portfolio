273cf3e14f727a3622bd109957a7b22b3c02f4fb
ArsalaanSuleman <105915521+ArsalaanSuleman@users.noreply.github.com>
2023-06-29 22:55:11 +0200

    chnage

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <React.Fragment>
+          <p>
+            Information technology student at Høyskolen Kristiania, following my
+            dream of becoming a software engineer/software developer.
+          </p>
+        </React.Fragment>
+      ),
+    },
+  ],
+
+  2: [
+    {
+      title: "2024 Bachelors Degree",
+      content: (
+        <React.Fragment>
+          <p></p>
+        </React.Fragment>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
