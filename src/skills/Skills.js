e49c635ada5f027f6f0e7dd04526e116191c4887
ArsalaanSuleman <105915521+ArsalaanSuleman@users.noreply.github.com>
2023-06-29 17:15:35 +0200

    fixed

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import Avatar from "../Avatar/Avatar.js";
+import SkillsMenu from "./SkillsMenu.js";
 
-export default class Skills extends Component {
+export default class About extends Component {
   render() {
-    return <div>Skills</div>;
+    return (
+      <React.Fragment>
+        <Avatar page="skills" />
+        <SkillsMenu />
+      </React.Fragment>
+    );
   }
 }
